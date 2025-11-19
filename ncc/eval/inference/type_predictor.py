f50cb7c13dd96f473a4bbae8200874461e1f0d8d
张昭骏 <89625845+Cicecoo@users.noreply.github.com>
2025-11-19 21:56:03 +0800

    fix

--- ncc/eval/inference/type_predictor.py
+++ ncc/eval/inference/type_predictor.py
@@ -4,7 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import torch
-from ncc.modules.seq2seq.ncc_incremental_decoder import NccIncrementalDecoder
+from ncc.modules.decoders.ncc_incremental_decoder import NccIncrementalDecoder
 
 
 class TypePredictor(object):
